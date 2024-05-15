6b313188c72975f2a21b9d7bcf70955ac85a9cdd
Andrii Roshkovan <andrii.roshkovan@gmail.com>
2024-05-15 18:31:13 +0200

    Graph for the 2nd lab of math III is of right proportions and outputs its' parameters as text. C version is tuned: float_t removed, area doubled (left and right of y axis), added calculations for area 80k, as in the python code.

--- matheIII/2.c
+++ matheIII/2.c
@@ -5,52 +5,46 @@
 #define PRINT_EDGE_VALUES 0
 
 uint16_t d = 100;
-uint16_t x = 10000;
+uint16_t x = 5000;
+uint16_t x_2 = 400;
 uint32_t total_area = 0;
+uint32_t total_area_2 = 0;
 
 uint16_t q_x = 0;
 uint8_t q_y = 0;
 
 // edge_x = 0; // from 0 to x
-float_t edge_y = 0; // from 0 to d
+float edge_y = 0; // from 0 to d
 
 uint32_t n_points = 0;
+uint32_t n_points_2 = 0;
 float area = 0;
+float area_2 = 0;
 
 // circle: x**2 + y**2 = (d/2)**2
 
-void input_values(){
-    printf("x = ");
-    scanf("%i", &q_x);
-    printf("y = ");
-    scanf("%i", &q_y);
-}
-
-void print_result(){
-    // printf("%f\n", edge_y);
-    printf("\nThe allowed range is %i\n", edge_y);
-
-    if(q_y <= edge_y){
-        printf("The point is in allowed range.");
-    }else{
-        printf("The point is beyond the allowed range.");
-    }
-}
-
 void print_area(){
+    printf("\nThe total area is %d * %d = %d\n", x_2, d, total_area_2);
+    printf("The total number of points is %d\n", n_points_2);
+    printf("The allowed fraction area for Q(X) is %f\n\n of 1", area_2);
+
     printf("The total area is %d * %d = %d\n", x, d, total_area);
     printf("The total number of points is %d\n", n_points);
-    printf("The allowed area for Q(X) is %f", area);
+    printf("The allowed fraction of area for Q(X) is %f\n of 1", area);
 }
 
 void main(){
     // input_values();
-    total_area = x * d;
+    total_area = x * d * 2;
+    total_area_2 = x_2 * d * 2;
 
     for(int i = 0; i < x; i++){
         q_x = i;
-        edge_y = (float) (pow(d, 3) / (pow(q_x, 2) + pow(d, 2));
-        n_points += round(edge_y);
+        edge_y = (float) (pow(d, 3) / (pow(q_x, 2) + pow(d, 2)));
+        n_points += round(edge_y * 2);
+        if(i == x_2){
+            n_points_2 = n_points;
+        }
         #if PRINT_EDGE_VALUES==1
             if(edge_y > 0){
                 printf("%d\n", edge_y);
@@ -60,6 +54,7 @@ void main(){
     // printf("%i", n_points);
     
     area = (float) n_points / total_area;
+    area_2 = (float) n_points_2 / total_area_2;
     print_area();
     // print_result();
 }
--- matheIII/function_graph.py
+++ matheIII/function_graph.py
@@ -1,14 +1,28 @@
 from manim import *
 
-class ExampleFunctionGraph(Scene):
+class Graph(Scene):
     d = 100
     x = 400
     y_edges = []
+    all_area = d * x * 2
+    allowed_area = 0
 
-    def draw_curved(self, range_val, diff, axes):
+    def draw_curved(self, axes):
         prev_q_y = -100
-        for q_x in range(0, range_val, diff):
+        diff = 10
+        q_y_text = Text(text="Aktuelles y: ",
+                        font_size=18,
+                        font="Arial").shift(LEFT * 4.8 + UP * 1.2)
+        allowed_area_text = Text(text="Der erlaubte Anteil der Fläche: ",
+                                 font_size=18,
+                                 font="Arial").shift(LEFT * 3.7 + UP * 0.9)
+        self.play(Create(q_y_text))
+        self.play(Create(allowed_area_text))
+        for q_x in range(0, self.x, diff):
             q_y = -((self.d**3) / ((q_x**2) + (self.d**2)))
+            self.allowed_area -= q_y * 2 * diff
+            allowed_area_value_text = Text(text=str(int(self.allowed_area)), font_size=18, font="Arial").next_to(allowed_area_text)
+            q_y_value_text = Text(text=str(int(q_y)), font_size=18, font="Arial").next_to(q_y_text)
 
             dot1 = Dot(axes.c2p(q_x, q_y), radius=0.04)
             dot2 = Dot(axes.c2p(-q_x, q_y), radius=0.04)
@@ -33,6 +47,8 @@ class ExampleFunctionGraph(Scene):
             self.add(area)
             self.add(line6)
             self.add(line7)
+            self.add(q_y_value_text)
+            self.add(allowed_area_value_text)
             self.wait(4 / diff)
             self.remove(line1)
             self.remove(line2)
@@ -40,28 +56,34 @@ class ExampleFunctionGraph(Scene):
             self.remove(line4)
             self.remove(line5)
             self.remove(area)
-            self.add(line6)
-            self.add(line7)
+            if q_x >= self.x - diff:
+                self.wait(5)
+            self.remove(q_y_value_text)
+            self.remove(allowed_area_value_text)
 
     def construct(self):
 
 
         axes = Axes(y_range=(-150, 150, 50),
-                    x_range=(-400, 400, 100),
-                    y_length=3,
-                    x_length=22,
+                    x_range=(-400, 400, 50),
+                    y_length=9,
+                    x_length=24,
                     ).add_coordinates()
 
         self.play(Create(axes))
 
-        line1 = Line(start=LEFT*10+DOWN, end=RIGHT*10+DOWN, color=RED)
+        line1 = Line(start=LEFT*10+DOWN*3, end=RIGHT*10+DOWN*3, color=RED)
         line2 = Line(start=LEFT*10, end=RIGHT*10, color=RED)
+        rectangle = Rectangle(height=0.5, width=1, color=BLUE, fill_color=BLUE, fill_opacity=0.5).shift(UP*2, LEFT*5)
+        text = Text(text="Das Bereich\nder erlaubten Punkten", font_size=18, font="Arial").next_to(rectangle)
+        all_area_text = Text(text="Die Gesamtfläche:  " + str(self.all_area), font_size=18, font="Arial").shift(UP*1.5, LEFT*4)
         self.play(Create(line1))
         self.play(Create(line2))
 
-        circle = Circle(color=GREEN, radius=0.5).shift(DOWN*0.5)
+        circle = Circle(color=GREEN, radius=1.5).shift(DOWN*1.5)
         self.play(Create(circle))
+        self.play(DrawBorderThenFill(rectangle))
+        self.play(Create(text))
+        self.play(Create(all_area_text))
 
-        self.draw_curved(self.x, 10, axes)
-
-        self.wait(5)
+        self.draw_curved(axes)
