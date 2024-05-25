c408040bbc657962a1ac8a4b3f4b183d868ee985
Andrii Roshkovan <andrii.roshkovan@gmail.com>
2024-05-25 23:13:38 +0200

    Added a plot for EHP (DotA 2 Armour), added previous wrong plots (in which the factor times 0.06 in the denominator was not an absolute value).

--- dota_2_armour.py
+++ dota_2_armour.py
@@ -1,34 +1,63 @@
 import matplotlib.pyplot as plt
+from matplotlib import style
 import numpy as np
+import pandas as pd
 
 HP = 700
 DMG = 62
 
 arm_vals = []
 dmg_factor_vals = []
+dmg_factor_wrong_vals = []
 dmg_done_vals = []
+dmg_done_wrong_vals = []
 hits_to_kill_vals = []
-
+hits_to_kill_wrong_vals = []
+ehp_vals = []
+ehp_wrong_vals = []
 
 
 def dmg_factor(armour):
-    return (1-((0.06*armour)/(1+00.6*armour)))
+    return 1-(0.06*armour)/(1+0.06*abs(armour))
 
-for armour in range(0, 50):
+def dmg_factor_wrong(armour):
+    return 1-(0.06*armour)/(1+0.06*armour)
+
+for armour in range(-50, 50):
     arm_vals.append(armour)
     dmg_factor_vals.append(dmg_factor(armour))
+    dmg_factor_wrong_vals.append(dmg_factor_wrong(armour))
     dmg_done_vals.append(DMG * dmg_factor(armour))
     hits_to_kill_vals.append(HP / dmg_done_vals[-1])
+    # hits_to_kill_wrong_vals.append(HP / dmg_done_wrong_vals[-1])
+    ehp_vals.append(HP / dmg_factor(armour))
+    ehp_wrong_vals.append(HP / dmg_factor_wrong(armour))
 
-# plt.style.use('seaborn')
-fig, ax = plt.subplots(nrows=1, ncols= 3, figsize=(15,5))
-ax[0].plot(arm_vals, dmg_factor_vals, label="Damage factor", color="#FF8855")
-ax[1].plot(arm_vals, dmg_done_vals, label="Damage done", color="#FF0000")
-ax[2].plot(arm_vals, hits_to_kill_vals, label="Hits to kill", color="#01FF08")
+plt.style.use('seaborn-v0_8-dark')
+fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(12,10))
+ax[0][0].plot(arm_vals, dmg_factor_vals, label="Damage factor", color="#FF8855")
+ax[0][0].plot(arm_vals, dmg_factor_wrong_vals, label="Damage factor - wrong", color="#FF22AA")
+ax[0][0].plot(arm_vals, [1 for i in range(-50,50)], color="#0000FF")
+ax[0][0].plot(arm_vals, [0 for i in range(-50,50)], color="#0000FF")
+
+ax[0][1].plot(arm_vals, dmg_done_vals, label="Damage done", color="#FF0000")
+ax[0][1].plot(arm_vals, [DMG for i in range(-50,50)], color="#0000FF")
+ax[0][1].plot(arm_vals, [0 for i in range(-50,50)], color="#0000FF")
+
+ax[1][0].plot(arm_vals, hits_to_kill_vals, label="Hits to kill", color="#01FF08")
+# ax[1][0].plot(arm_vals, hits_to_kill_wrong_vals, label="Hits to kill - wrong", color="#A1FF06")
+ax[1][0].plot(arm_vals, [HP/DMG for i in range(-50,50)], color="#0000FF")
+ax[1][0].plot(arm_vals, [0 for i in range(-50,50)], color="#0000FF")
+
+ax[1][1].plot(arm_vals, ehp_vals, label="EHP", color="#00AAFF")
+ax[1][1].plot(arm_vals, ehp_wrong_vals, label="EHP - wrong", color="#11AA11")
+ax[1][1].plot(arm_vals, [HP for i in range(-50,50)], color="#0000FF")
+ax[1][1].plot(arm_vals, [0 for i in range(-50,50)], color="#0000FF")
 
 for a in ax:
-    a.legend()
-    # a.set_aspect(1)
+    a[0].legend()
+    a[1].legend()
+    # a.set_aspect()
 plt.tight_layout()
 plt.show()
 
