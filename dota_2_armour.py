448d0dd7eb17c8f47ee176765cce856c68533e33
Andrii Roshkovan <andrii.roshkovan@gmail.com>
2024-05-26 22:17:27 +0200

    DotA 2 Armour: added denominator and numerator graphs for the damage multiplier.

--- dota_2_armour.py
+++ dota_2_armour.py
@@ -7,14 +7,22 @@ HP = 700
 DMG = 62
 
 arm_vals = []
+
 dmg_factor_vals = []
 dmg_factor_wrong_vals = []
+
 dmg_done_vals = []
 dmg_done_wrong_vals = []
+
 hits_to_kill_vals = []
 hits_to_kill_wrong_vals = []
+
 ehp_vals = []
 ehp_wrong_vals = []
+ehp_numerator = []
+ehp_denominator_right = []
+ehp_denominator_wrong = []
+
 
 
 def dmg_factor(armour):
@@ -32,13 +40,21 @@ for armour in range(-50, 50):
     # hits_to_kill_wrong_vals.append(HP / dmg_done_wrong_vals[-1])
     ehp_vals.append(HP / dmg_factor(armour))
     ehp_wrong_vals.append(HP / dmg_factor_wrong(armour))
+    ehp_numerator.append(0.06*(armour))
+    ehp_denominator_right.append(1+0.06*abs(armour))
+    ehp_denominator_wrong.append(1+0.06*armour)
 
 plt.style.use('seaborn-v0_8-dark')
-fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(12,10))
+fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(12, 10))
+ax[0][0].set_ylim(-2, 2)
+# ax[0][0].set_aspect('equal', 'box')
 ax[0][0].plot(arm_vals, dmg_factor_vals, label="Damage factor", color="#FF8855")
 ax[0][0].plot(arm_vals, dmg_factor_wrong_vals, label="Damage factor - wrong", color="#FF22AA")
-ax[0][0].plot(arm_vals, [1 for i in range(-50,50)], color="#0000FF")
-ax[0][0].plot(arm_vals, [0 for i in range(-50,50)], color="#0000FF")
+ax[0][0].plot(arm_vals, ehp_numerator, label="EHP - numberator", color="#040F13")
+ax[0][0].plot(arm_vals, ehp_denominator_wrong, label="EHP - wrong denominator", color="#11A011")
+ax[0][0].plot(arm_vals, ehp_denominator_right, label="EHP - true denominator", color="#BB0011", linestyle="--")
+ax[0][0].plot(arm_vals, [1 for i in range(-50,50)], color="#0000FF", linestyle="--")
+ax[0][0].plot(arm_vals, [0 for i in range(-50,50)], color="#0000FF",  linestyle="--")
 
 ax[0][1].plot(arm_vals, dmg_done_vals, label="Damage done", color="#FF0000")
 ax[0][1].plot(arm_vals, [DMG for i in range(-50,50)], color="#0000FF")
